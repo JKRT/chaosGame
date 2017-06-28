aa7dac338c5f139bcca009641a5916801efacb30
mikbo283 <mikael.boors@gmail.com>
2017-06-29 01:49:44 +0200

    Added Barnsley Fern

--- chaosGame.py
+++ chaosGame.py
@@ -1,6 +1,6 @@
 from graphics import *                                                      
 from random import randint
-
+import numpy as np
 
 
 def main():
@@ -9,6 +9,7 @@ def main():
     colour_mode = raw_input("Enter colour mode: \t") # Night, Day
     win = GraphWin("chaosGame", 1024, 1024)
     object_colour = 'black'
+
     
     if colour_mode == "Night":
         win.setBackground('black')
@@ -27,6 +28,8 @@ def main():
         pentagon(win,space_pentagon, object_colour)
     elif mode == "hexagon":
         hexagon(win, object_colour)
+    elif mode == "fern":
+        Barnsley_Fern(win, object_colour)
 
 
 def compare_points(p1,p2):
@@ -137,7 +140,7 @@ def square(win, colour):
 
     current_point = point_placement(original_points,
                                     Circle(Point(0,0),0.0001),
-                                    number_of_points,win)
+                                    number_of_points,win, colour)
 
     #Temporary values before the loop
 
@@ -222,9 +225,57 @@ def draw_cordinate_system(win):
             stepLineX.draw(win)
             stepLineY.draw(win)
         
-        
 
+############# Barnsley Fern #############
+            
+def f_1(x_array):
+    A = np.array([[0,0],[0,0.16]])
+    y_array = np.dot(A,x_array)
+    return y_array
     
+def f_2(x_array):
+    A = np.array([[0.85,0.04],[-0.04,0.85]])
+    b = np.array([0,1.6])
+    y_array = np.dot(A,x_array) + b
+    return y_array
 
+def f_3(x_array):
+    A = np.array([[0.2,-0.26],[0.23,0.22]])
+    b = np.array([0,1.6])
+    y_array = np.dot(A,x_array) + b
+    return y_array
 
+def f_4(x_array):
+    A = np.array([[-0.15,0.28],[0.26,0.24]])
+    b = np.array([0,0.44])
+    y_array = np.dot(A,x_array) + b
+    return y_array
+
+def Cartesian_to_Circle(x_array):
+    origin = np.array([512,800])
+    trans = np.array([[70,0],[0,70]])
+    y_array = origin - np.dot(trans,x_array)
+    c = Circle(Point(y_array[0],y_array[1]),0.0001)
+    return c
+    
+def Barnsley_Fern(win, colour):
+    cart = np.array([0,0])
+
+    for i in range(0,10000000):
+        p = Cartesian_to_Circle(cart)
+        p.setOutline(colour)
+        p.setFill(colour)
+        p.draw(win)
+
+        r = np.random.uniform(0,100)
+
+        if r <= 1:
+            cart = f_1(cart)
+        elif r <= 86:
+            cart = f_2(cart)
+        elif r <= 93:
+            cart = f_3(cart)
+        else:
+            cart = f_4(cart)
+    
 main()
